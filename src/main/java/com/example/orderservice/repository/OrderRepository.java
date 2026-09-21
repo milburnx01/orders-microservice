c49deff70ac35e039476793cf9a05d6d007ca036
andrey anisimov <sendtoanisimov@gmail.com>
2026-09-21 14:47:52 +0300

    add repositories and mappers

---
+++ src/main/java/com/example/orderservice/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.example.orderservice.repository;
+
+import com.example.orderservice.entity.Order;
+import com.example.orderservice.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Page<Order> findAllByUser(User user, Pageable pageable);
+}
